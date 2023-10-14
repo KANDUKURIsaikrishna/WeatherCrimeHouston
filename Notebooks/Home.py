ea5b4a31aabd6006a1fc5c1ed7f21ee920cdef81
Chin Hao Zac <zac.chin@hotmail.com>
2023-10-14 13:02:57 +0000

    Add home page

---
+++ Notebooks/Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import os
+# This is the main streamlit app file
+print(os.system('pwd'))
+
+st.set_page_config(page_title='Discovering the Relationship Between Crime Statistics and Weather in the Houston Area', layout = 'wide')
+
+
+st.title('Discovering the Relationship Between Crime Statistics and Weather in the Houston Area')
+
+st.header('Project background')
+
+
+st.markdown(
+'''
+## [Omdena  Houston, USA Chapter](https://omdena.com/chapter-challenges/discover-the-relationship-between-crime-statistics-and-weather-in-the-houston-area/)
+'''
+)
+
+
+
+
