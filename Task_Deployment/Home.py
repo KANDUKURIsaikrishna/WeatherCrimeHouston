821b3c18ddc782b246bf36d2f7e2fa785009e375
Kandukuri saikrishna <50510272+KANDUKURIsaikrishna@users.noreply.github.com>
2023-10-28 14:54:14 +0200

    Update Home.py

--- Task_Deployment/Home.py
+++ Task_Deployment/Home.py
@@ -8,9 +8,23 @@ st.set_page_config(page_title='Discovering the Relationship Between Crime Statis
 
 st.title('Discovering the Relationship Between Crime Statistics and Weather in the Houston Area')
 
-st.header('Project background')
+st.title("Mission and Vision Statements")
+mission_statement = """
+Mission:
+- Promote real-world AI through running open-source projects
+- Provide case study-based education
+- Provide AI services to local AI enthusiasts and businesses around the world
+- Host offline events
+"""
 
-
+vision_statement = """
+Vision:
+- Collaborate
+- Network
+- Deliver
+"""
+st.markdown(mission_statement, unsafe_allow_html=True)
+st.markdown(vision_statement, unsafe_allow_html=True)
 st.markdown(
 '''
 ## [Omdena  Houston, USA Chapter](https://omdena.com/chapter-challenges/discover-the-relationship-between-crime-statistics-and-weather-in-the-houston-area/)
